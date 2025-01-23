768fa777d7db80a763e54ece42f3595d7a949a6e
LAIBA SHEHZAD <ls2092@hw.ac.uk>
2025-01-23 12:15:29 +0400

    suggestions

--- src/Suggestions.tsx
+++ src/Suggestions.tsx
@@ -1,14 +1,52 @@
 function Suggestions() {
     return(
         <>
-        <div className="Suggestions-Container">
+        
 
             <div className="main-headings">
-                <p>Energy Saving</p>
-                <p>Security</p>
-                <p >Automation</p>
+                <button id="energy-saving">Energy Saving</button>
+                <button id="automation">Automation</button>
+                <button id="security">Security</button>
+            </div>
+        
+    <div className="boxes">   
+        <div className="energy-box">
+            <p>suggetsions</p>
+            <div className="sliding-boxes"> 
+                <div className="suggestions-boxes"></div>
+                <div className="suggestions-boxes2"></div>
+                <div className="suggestions-boxes3"></div>
+                <div className="suggestions-boxes3"></div>
+            
             </div>
         </div>
+
+        <div className="auto-box">
+            <p>suggetsions</p>
+            <div className="sliding-boxes"> 
+                <div className="suggestions-boxes"></div>
+                <div className="suggestions-boxes2"></div>
+                <div className="suggestions-boxes3"></div>
+                <div className="suggestions-boxes3"></div>
+            
+            </div>
+            
+            
+        </div>
+
+        <div className="security-box">
+            <p>suggetsions</p>
+            <div className="sliding-boxes"> 
+                <div className="suggestions-boxes"></div>
+                <div className="suggestions-boxes2"></div>
+                <div className="suggestions-boxes3"></div>
+                <div className="suggestions-boxes3"></div>
+            
+            </div>
+            
+            
+        </div>
+    </div> 
         </>
     )
 }
