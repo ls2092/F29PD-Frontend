0a0e2b482f6e30ced62d69e8bba9cc36dddb4990
razinmohammedd <145095526+razinmohammedd@users.noreply.github.com>
2025-01-27 22:02:21 +0400

    Added Login Page

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,11 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import Login from './Login';
-import './index.css'
-import App from './App.tsx'
+import Login from './Login.tsx'
+import './Login.css'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <Login />
   </StrictMode>,
 )
 
