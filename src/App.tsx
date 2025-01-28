9bd1bd9a576c8dd73be0b38a2734116160e17b49
LAIBA SHEHZAD <ls2092@hw.ac.uk>
2025-01-28 19:18:16 +0400

    login page

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+/*import React, { useState, useEffect } from 'react';
 import SideBarNav from './SideBarNav';
 import EnergyConsumed from './EnergyConsumed';
 import Settings from './SettingsPage';
@@ -30,7 +30,7 @@ function App() {
       case '#Home':
         return <EnergyConsumed />;
       case '#SettingsPage':
-        return <Settings />;
+        return <Settings />
       case '#Suggestions':
         return <Suggestions />;
       case '#EnergyConsumed':
@@ -47,6 +47,67 @@ function App() {
         return <EnergyConsumed />;
     }
   };
+
+  return (
+    <>
+      <SideBarNav />
+      {renderPage()}
+    </>
+  );
+}
+
+export default App;*/
+
+import React, { useState, useEffect } from 'react';
+import SideBarNav from './SideBarNav';
+import EnergyConsumed from './EnergyConsumed';
+import Settings from './SettingsPage';
+import Suggestions from './Suggestions';
+import AddDevice from './AddDevice';
+import Profile from './Profiles';
+import Automation from './Automation';
+import Info from './info';
+import Login from './Login';
+
+function App() {
+  const [isAuthenticated, setIsAuthenticated] = useState<boolean>(false);
+  const [currentPage, setCurrentPage] = useState<string>(window.location.hash || '#');
+
+  useEffect(() => {
+    const handleHashChange = () => {
+      setCurrentPage(window.location.hash || '#');
+    };
+
+    window.addEventListener('hashchange', handleHashChange);
+    return () => {
+      window.removeEventListener('hashchange', handleHashChange);
+    };
+  }, []);
+
+  const renderPage = () => {
+    switch (currentPage) {
+      case '#Home':
+        return <EnergyConsumed />;
+      case '#SettingsPage':
+        return <Settings />;
+      case '#Suggestions':
+        return <Suggestions />;
+      case '#AddDevice':
+        return <AddDevice />;
+      case '#Automation':
+        return <Automation />;
+      case '#Info':
+        return <Info />;
+      case '#Profile':
+        return <Profile />;
+      default:
+        return <EnergyConsumed />;
+    }
+  };
+
+  if (!isAuthenticated) {
+    return <Login onLogin={() => setIsAuthenticated(true)} />;
+  }
 
   return (
     <>
--- src/SettingsPage.tsx
+++ src/SettingsPage.tsx
@@ -1,5 +1,8 @@
 function Settings() {
-  
+  return
+  <>
+  <h1>settings page</h1>
+  </>
   }
   
 export default Settings;
